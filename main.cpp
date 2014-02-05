096c0a139d57228f7d95659ecffd8abf9ef1185a
Vitor Shade <vitor.shade@gmail.com>
2014-02-05 12:40:55 -0200

    -Test commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     Model m;
     int ret = m.Load("test_model/capsule.obj");
     return ret;
+#test
 //    QApplication a(argc, argv);
 //    MainWindow w;
 //    w.show();
